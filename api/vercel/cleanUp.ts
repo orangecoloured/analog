9488d0bb238bb47da39116e01ffa736a1469e46e
Alexander Petcoglo <mail@rckt.cc>
2025-06-04 21:02:02 +0200

    vercel

--- api/vercel/cleanUp.ts
+++ api/vercel/cleanUp.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { cleanUpOldData } from "../cleanUp";
+import { cleanUpOldData } from "../cleanUp.ts";
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   await cleanUpOldData();
