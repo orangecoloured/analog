42e458ff039a86485abc58c0bb4b1b2862b3067d
Alexander Petcoglo <mail@rckt.cc>
2025-06-04 11:26:07 +0200

    vercel

---
+++ api/get.js
@@ -0,0 +1,23 @@
+import { getData } from "../src/api/get";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  const token = req.query.token;
+
+  if (process.env.VITE_ANALOG_GET_TOKEN && token !== process.env.VITE_ANALOG_GET_TOKEN) {
+    return res.status(200).json({});
+  }
+
+  try {
+    const data = await getData();
+
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(500).setHeader("Content-Type", "text/plain");
+  
+    return res.send(`Internal Server Error: ${error instanceof Error ? error.message : error}`);
+  }
+};
