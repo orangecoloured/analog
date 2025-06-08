7f4ab6a50084bcfea1c00ab1abb1d8a9ab564614
Alexander Petcoglo <mail@rckt.cc>
2025-06-08 21:25:25 +0200

    vercel changes

--- api/get.ts
+++ api/get.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { getData } from "../src/services/redis";
+import { getData } from "../src/services/redis/get.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "GET") {
