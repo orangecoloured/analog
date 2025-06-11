50bcef108ce85dd020dabd6ab978274509b480a2
Alexander Petcoglo <mail@rckt.cc>
2025-06-11 10:53:20 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type UserConfig } from "vite";
 import netlify from "@netlify/vite-plugin";
 import { PORT_DEV } from "./src/utils";
-
+console.log(1);
 const baseConfig = {
   root: ".",
   publicDir: "public",
@@ -9,28 +9,28 @@ const baseConfig = {
     outDir: "dist",
     emptyOutDir: true,
   },
-}
+};
 
 const platformSpecificConfig = (config: UserConfig) => {
   switch (true) {
-    case (!!process.env.NETLIFY): {
+    case !!process.env.NETLIFY: {
       return {
         ...config,
         plugins: [netlify()],
-      }
+      };
     }
 
-    case (!!process.env.VERCEL): {
+    case !!process.env.VERCEL: {
       return config;
     }
 
     default:
       return config;
   }
-}
+};
 
 export default defineConfig(({ command }) => {
-  if (command === 'serve') {
+  if (command === "serve") {
     const port = parseInt(process.env.ANALOG_PORT_SERVER as string, 10);
 
     return {
@@ -39,10 +39,10 @@ export default defineConfig(({ command }) => {
         port,
         proxy: {
           "/api": `http://localhost:${isNaN(port) ? PORT_DEV + 1 : port}`,
-        }
-      }
+        },
+      },
     };
   } else {
     return platformSpecificConfig(baseConfig);
   }
-})
+});
