2344f69e67a0f69cbac11a9e5ce13d58f842dffe
Alexander Petcoglo <mail@rckt.cc>
2025-06-13 22:04:52 +0200

    test redis connection

--- netlify/functions/events.ts
+++ netlify/functions/events.ts
@@ -61,6 +61,8 @@ export const handler: Handler = async (event) => {
       try {
         const data = await getData();
 
+        console.debug("Data was fetched successfully");
+
         return {
           statusCode: 200,
           headers: {
