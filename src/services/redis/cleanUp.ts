f74f0eb4f1b1e1f6d611065a4767b3f1ede63a08
Alexander Petcoglo <mail@rckt.cc>
2025-06-15 09:03:51 +0200

    fix cleanup function

--- src/services/redis/cleanUp.ts
+++ src/services/redis/cleanUp.ts
@@ -1,5 +1,5 @@
 import { redis } from "./redis.js";
-import { REDIS_KEY_PREFIX } from "./contants.js";
+import { REDIS_KEY_PREFIX, REDIS_REQUEST_ITEMS_COUNT } from "./contants.js";
 import { TIME_RANGE_MAX } from "../../utils/constants.js";
 
 export const cleanUpOldData = async () => {
@@ -9,16 +9,26 @@ export const cleanUpOldData = async () => {
     offset = TIME_RANGE_MAX;
   }
 
-  const cutoff = Date.now() - ((offset + 1) * 24 * 60 * 60 * 1000);
+  const cutoff = Date.now() - (offset + 1) * 24 * 60 * 60 * 1000;
   const pattern = `${REDIS_KEY_PREFIX}:*`;
   let cursor = "0";
+  const requestItemsCount = parseInt(
+    process.env.ANALOG_REDIS_REQUEST_ITEM_COUNT as string,
+    10,
+  );
 
   do {
-    const [nextCursor, keys] = await redis.scan(cursor, "MATCH", pattern, "COUNT", 100);
+    const [nextCursor, keys] = await redis.scan(
+      cursor,
+      "MATCH",
+      pattern,
+      "COUNT",
+      isNaN(requestItemsCount) ? REDIS_REQUEST_ITEMS_COUNT : requestItemsCount,
+    );
     cursor = nextCursor;
 
     for (const key of keys) {
       await redis.zremrangebyscore(key, 0, cutoff);
     }
   } while (cursor !== "0");
-}
+};
